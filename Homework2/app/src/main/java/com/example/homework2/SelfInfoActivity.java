04811486904e0e5a02e6783e66ae182a4ae1c42b
煜豪 林 <1156641208@qq.com>
2022-03-20 10:47:39 +0800

    null

---
+++ Homework2/app/src/main/java/com/example/homework2/SelfInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelfInfoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_info);
+    }
+}
