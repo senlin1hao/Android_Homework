9cdffe788696726c255fe6695f66e6e552963faf
sen lin1hao <1156641208@qq.com>
2022-03-26 18:13:30 +0800

    null

---
+++ Homework3/app/src/main/java/com/example/homework3/RecyclerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.homework3;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>
+{
+    private List<app> appList;
+
+    public RecyclerAdapter(List<app> appList)
+    {
+        this.appList = appList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item, parent, false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position)
+    {
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private final TextView appName;
+        private final ImageView appImage;
+
+        public ViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+        }
+    }
+}
---
+++ Homework3/app/src/main/java/com/example/homework3/app.java
@@ -0,0 +1,33 @@
+package com.example.homework3;
+
+public class app
+{
+    private String name;
+    private int imageId;
+
+    public app(String name, int imageId)
+    {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getImageId()
+    {
+        return imageId;
+    }
+
+    public void setImageId(int imageId)
+    {
+        this.imageId = imageId;
+    }
+}
